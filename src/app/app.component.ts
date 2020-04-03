db2a251c5a77afe842925ea39a61cf74cf3389fc
bculo <culobozo@gmail.com>
2020-04-03 15:08:30 +0200

    EventEmitter replaced with Subject

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,11 +6,4 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  loadedFeature = 'recipe';
-  
-  title = 'angular-app';
-
-  onNavigate(feature: string){
-    this.loadedFeature = feature;
-  }
 }
